51519fcfa5fda7f326b233a7c5a29118c8cdd8df
giorgi568 <mateshvili02@mail.ru>
2024-03-22 02:57:35 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+  })
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
