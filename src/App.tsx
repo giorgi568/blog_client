117267d6f6c652eaa295ade5c4a516cc3514f018
giorgi568 <mateshvili02@mail.ru>
2024-03-25 02:33:20 +0400

    got posts to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,69 @@
-// import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { DateTime } from 'luxon';
 
-function App() {
-  useEffect(() => {
-  })
-
-  return (
-    <>
-    </>
-  )
+async function fetchPosts() {
+  try {
+    const posts = await fetch('https://furry-leeward-ricotta.glitch.me/posts', {
+      mode: 'cors',
+    });
+    if (!posts.ok) {
+      throw new Error('Failed to fetch posts');
+    }
+    const parsedPosts = await posts.json();
+    console.log(parsedPosts.posts);
+    return parsedPosts.posts;
+  } catch (err) {
+    console.error('Error fetching posts:', err);
+  }
 }
 
-export default App
+interface postObj {
+  title?: string;
+  author?: authorObj;
+  published?: boolean;
+  text?: string;
+  timestamp?: string;
+}
+interface authorObj {
+  username?: string;
+}
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function getPosts() {
+      const fetchedPosts = await fetchPosts();
+      setPosts(fetchedPosts);
+    }
+    getPosts();
+  }, []);
+
+  return (
+    <div className='content'>
+      {posts.map((post: postObj, index: number) => {
+        return (
+          <div key={index} className='postCard'>
+            <h3>{post.title}</h3>
+            <p>{post.text}</p>
+            {typeof post.author !== 'undefined' ? (
+              <em>{post.author?.username}</em>
+            ) : (
+              'unknown'
+            )}
+            <br />
+            
+            {/* <em>{post.timestamp}</em> */}
+            <em>
+              {DateTime.fromISO(post.timestamp).toLocaleString(
+                DateTime.DATETIME_MED
+              )}
+            </em>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
